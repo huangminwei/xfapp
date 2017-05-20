63bfd21b0d382c6538c05afd44cd96d58331ac56
kris <120025737@qq.com>
2017-05-20 16:46:22 +0800

    ajust code \

--- clientauth/src/Qrcode.php
+++ clientauth/src/Qrcode.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace apanly\BrowserDetector;
+namespace clientauth;
 
 /**
  * Browser Detection.
---
+++ xfapp/src/Qrcode.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace xfapp;
+
+/**
+ * Browser Detection.
+ */
+class Qrcode
+{
+//    const UNKNOWN = 'unknown';
+
+    public static function test()
+    {
+        echo 11;
+    }
+
+}
